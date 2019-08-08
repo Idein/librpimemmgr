1e6f63aa341ea9139546e48ec7240da67226f889
Sugizaki Yukimasa <ysugi@idein.jp>
2019-08-08 16:14:19 +0900

    Support for VCSM on CMA

--- test/addr.c
+++ test/addr.c
@@ -15,76 +15,139 @@
 #include <sys/types.h>
 #include <inttypes.h>
 
-static int test_vcsm(const VCSM_CACHE_TYPE_T cache_type, struct rpimemmgr *sp)
+static int test_vcsm_gpu(const VCSM_CACHE_TYPE_T cache_type)
 {
     uint32_t busaddr;
     int err;
-
-    err = rpimemmgr_alloc_vcsm(4096, 4096, cache_type, NULL, &busaddr, sp);
-    if (err)
-        return err;
-
-    printf("busaddr=0x%08" PRIx32 "\n", busaddr);
-
-    return rpimemmgr_free_by_busaddr(busaddr, sp);
-}
-
-static int test_mailbox(const uint32_t flags, struct rpimemmgr *sp)
-{
-    uint32_t busaddr;
-    int err;
-
-    err = rpimemmgr_alloc_mailbox(4096, 4096, flags, NULL, &busaddr, sp);
-    if (err)
-        return err;
-
-    printf("busaddr=0x%08" PRIx32 "\n", busaddr);
-
-    return rpimemmgr_free_by_busaddr(busaddr, sp);
-}
-
-int main(void)
-{
     struct rpimemmgr st;
-    int err;
 
     err = rpimemmgr_init(&st);
     if (err)
         return err;
 
-    printf("VCSM:    NONE:             ");
-    err = test_vcsm(VCSM_CACHE_TYPE_NONE,        &st);
-    if (err)
-        return err;
-    printf("VCSM:    HOST:             ");
-    err = test_vcsm(VCSM_CACHE_TYPE_HOST,        &st);
-    if (err)
-        return err;
-    printf("VCSM:    VC:               ");
-    err = test_vcsm(VCSM_CACHE_TYPE_VC,          &st);
-    if (err)
-        return err;
-    printf("VCSM:    HOST_AND_VC:      ");
-    err = test_vcsm(VCSM_CACHE_TYPE_HOST_AND_VC, &st);
+    err = rpimemmgr_alloc_vcsm(4096, 4096, cache_type, NULL, &busaddr, &st);
     if (err)
         return err;
 
-    printf("Mailbox: NORMAL:           ");
-    err = test_mailbox(MEM_FLAG_NORMAL,           &st);
-    if (err)
-        return err;
-    printf("Mailbox: DIRECT:           ");
-    err = test_mailbox(MEM_FLAG_DIRECT,           &st);
-    if (err)
-        return err;
-    printf("Mailbox: COHERENT:         ");
-    err = test_mailbox(MEM_FLAG_COHERENT,         &st);
-    if (err)
-        return err;
-    printf("Mailbox: L1_NONALLOCATING: ");
-    err = test_mailbox(MEM_FLAG_L1_NONALLOCATING, &st);
+    printf("busaddr=0x%08" PRIx32 "\n", busaddr);
+
+    err = rpimemmgr_free_by_busaddr(busaddr, &st);
     if (err)
         return err;
 
     return rpimemmgr_finalize(&st);
+}
+
+#ifdef RPIMEMMGR_VCSM_HAS_CMA
+
+static int test_vcsm_cma(const VCSM_CACHE_TYPE_T cache_type)
+{
+    uint32_t busaddr;
+    int err;
+    struct rpimemmgr st;
+
+    err = rpimemmgr_init(&st);
+    if (err)
+        return err;
+
+    st.vcsm_use_cma = 1;
+
+    err = rpimemmgr_alloc_vcsm(4096, 4096, cache_type, NULL, &busaddr, &st);
+    if (err)
+        return err;
+
+    printf("busaddr=0x%08" PRIx32 "\n", busaddr);
+
+    err = rpimemmgr_free_by_busaddr(busaddr, &st);
+    if (err)
+        return err;
+
+    return rpimemmgr_finalize(&st);
+}
+
+#endif /* RPIMEMMGR_VCSM_HAS_CMA */
+
+static int test_mailbox(const uint32_t flags)
+{
+    uint32_t busaddr;
+    int err;
+    struct rpimemmgr st;
+
+    err = rpimemmgr_init(&st);
+    if (err)
+        return err;
+
+    err = rpimemmgr_alloc_mailbox(4096, 4096, flags, NULL, &busaddr, &st);
+    if (err)
+        return err;
+
+    printf("busaddr=0x%08" PRIx32 "\n", busaddr);
+
+    err = rpimemmgr_free_by_busaddr(busaddr, &st);
+    if (err)
+        return err;
+
+    return rpimemmgr_finalize(&st);
+}
+
+int main(void)
+{
+    int err;
+
+    printf("VCSM (GPU):    NONE:             ");
+    err = test_vcsm_gpu(VCSM_CACHE_TYPE_NONE);
+    if (err)
+        return err;
+    printf("VCSM (GPU):    HOST:             ");
+    err = test_vcsm_gpu(VCSM_CACHE_TYPE_HOST);
+    if (err)
+        return err;
+    printf("VCSM (GPU):    VC:               ");
+    err = test_vcsm_gpu(VCSM_CACHE_TYPE_VC);
+    if (err)
+        return err;
+    printf("VCSM (GPU):    HOST_AND_VC:      ");
+    err = test_vcsm_gpu(VCSM_CACHE_TYPE_HOST_AND_VC);
+    if (err)
+        return err;
+
+#ifdef RPIMEMMGR_VCSM_HAS_CMA
+
+    printf("VCSM (CMA):    NONE:             ");
+    err = test_vcsm_cma(VCSM_CACHE_TYPE_NONE);
+    if (err)
+        return err;
+    printf("VCSM (CMA):    HOST:             ");
+    err = test_vcsm_cma(VCSM_CACHE_TYPE_HOST);
+    if (err)
+        return err;
+    printf("VCSM (CMA):    VC:               ");
+    err = test_vcsm_cma(VCSM_CACHE_TYPE_VC);
+    if (err)
+        return err;
+    printf("VCSM (CMA):    HOST_AND_VC:      ");
+    err = test_vcsm_cma(VCSM_CACHE_TYPE_HOST_AND_VC);
+    if (err)
+        return err;
+
+#endif /* RPIMEMMGR_VCSM_HAS_CMA */
+
+    printf("Mailbox: NORMAL:           ");
+    err = test_mailbox(MEM_FLAG_NORMAL);
+    if (err)
+        return err;
+    printf("Mailbox: DIRECT:           ");
+    err = test_mailbox(MEM_FLAG_DIRECT);
+    if (err)
+        return err;
+    printf("Mailbox: COHERENT:         ");
+    err = test_mailbox(MEM_FLAG_COHERENT);
+    if (err)
+        return err;
+    printf("Mailbox: L1_NONALLOCATING: ");
+    err = test_mailbox(MEM_FLAG_L1_NONALLOCATING);
+    if (err)
+        return err;
+
+    return 0;
 }
